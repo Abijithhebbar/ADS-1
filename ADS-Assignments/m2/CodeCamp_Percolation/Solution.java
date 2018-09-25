6b61bdad575abb3fcae6333bb43621698555e0a2
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-09-25 20:44:38 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-Assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-Assignments/m2/CodeCamp_Percolation/Solution.java
@@ -14,7 +14,7 @@ final class Solution {
 	/**
 	 main function.
 	 **/
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int input = Integer.parseInt(sc.nextLine());
 		percolate obj = new percolate(input);
@@ -27,17 +27,26 @@ final class Solution {
 	}
 
 }
-
+/**
+ * class.
+ **/
 class percolate {
 	boolean[][] grid;
 	WeightedQuickUnionUF obj;
 	int size;
-	percolate(int size) {
+	/**
+	 * @param size int.
+	 **/
+	percolate(final int size) {
 		this.size = size;
 		grid = new boolean[size][size];
 		obj = new WeightedQuickUnionUF(size * size + 2);
 	}
-	public void open(int row, int column) {
+	/**
+	 * @param row int.
+	 * @param column int.
+	 **/
+	public void open(final int row, final int column) {
 		if (grid[row][column]) {
 			return;
 		}
@@ -49,25 +58,36 @@ class percolate {
 			obj.union(convert(row, column), size * size + 1);
 		}
 		if (row < size - 1 && grid[row + 1][column]) { //bottom element
-			obj.union(convert(row, column), convert(row + 1, column));
+			obj.union(convert(row, column),
+				convert(row + 1, column));
 		}
 		if (row > 0 && grid[row - 1][column]) { //top element
-			obj.union(convert(row, column), convert(row - 1, column));
+			obj.union(convert(row, column),
+				convert(row - 1, column));
 		}
 		if (column > 0 && grid[row][column - 1]) { //left element
-			obj.union(convert(row, column), convert(row, column - 1));
+			obj.union(convert(row, column),
+			 convert(row, column - 1));
 		}
-		if (column < size - 1 && grid[row][column + 1]) { //right element
-			obj.union(convert(row, column), convert(row, column + 1));
+		if (column < size - 1 && grid[row][column + 1]) {
+			obj.union(convert(row, column),
+			 convert(row, column + 1));
 		}
 
 
 	}
+	/**
+	* @return true or false.
+	 **/
 	public boolean ispercolate() {
 		return obj.connected(size * size, size * size + 1);
 	}
-
-	public int convert(int i, int j) {
+/**
+ * @param i int.
+ * @param j int.
+ * @return i*size+j.
+ **/
+	public int convert(final int i, final int j) {
 		return i * size + j;
 	}
 }
