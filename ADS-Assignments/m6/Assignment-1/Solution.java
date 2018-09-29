72f7a3e8aaf31819deb5a0d5854d7d9ccba397fd
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-09-29 11:36:14 +0530

    "check_style_score -> 8 of 12 passed. style score: 0/1 "

--- ADS-Assignments/m6/Assignment-1/Solution.java
+++ ADS-Assignments/m6/Assignment-1/Solution.java
@@ -27,7 +27,21 @@ class AddLargeNumbers {
         long c = a + b;
         String result = c + "";
         return result;
-
+        // LinkedList add = new LinkedList();
+        // LinkedList add1 = new LinkedList();
+        // add.push(list1);
+        // add1.push(list2);
+        // String res1 = "";
+        // LinkedList result = new LinkedList();
+        // while (!add.isEmpty()) {
+        //     int res = 0;
+        //     while (!add1.isEmpty()) {
+        //        res = add.pop() + add1.pop();
+        //        res1 += res;
+        //     }
+        // }
+        // result.push(res1);
+        // return result.displayAll();
 
     }
 }
