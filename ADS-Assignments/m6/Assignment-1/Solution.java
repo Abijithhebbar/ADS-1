de80173483512d5a020e21cf463818fa2aa6f409
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-09-29 12:45:46 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS-Assignments/m6/Assignment-1/Solution.java
+++ ADS-Assignments/m6/Assignment-1/Solution.java
@@ -1,19 +1,42 @@
 import java.util.Scanner;
-class AddLargeNumbers {
+/**
+ add large number class.
+ **/
+final class AddLargeNumbers {
+    /**
+     * default constructor.
+     **/
+    private AddLargeNumbers() {
 
+    }
+/**
+ * number To digit.
+ * @param number obj.
+ * @return obj.
+ **/
     public static LinkedList numberToDigits(final String number) {
         LinkedList numberToDigits = new LinkedList();
-        for(int i = number.length()-1; i >= 0; i--) {
+        for (int i = number.length() - 1; i >= 0; i--) {
             String elementConverstion = "" + number.charAt(i);
             numberToDigits.push(Integer.parseInt(elementConverstion));
         }
         return numberToDigits;
     }
-
+/**
+ * digits to string.
+ * @param list obj.
+ * @return string.
+ */
     public static String digitsToNumber(final LinkedList list) {
         return list.toString();
     }
-
+/**
+ add large numbers
+ * @param list1 obj.
+ * @param list2 obj.
+ *
+ * @return object.
+ **/
     public static LinkedList addLargeNumbers(final LinkedList list1, final LinkedList list2) {
         LinkedList result = new LinkedList();
         int a, c = 0, b = 0;
@@ -25,7 +48,7 @@ class AddLargeNumbers {
             // a = 0;
             while (!list2.isEmpty()) {
                 a = list1.pop() + list2.pop() + b;
-                 // c = 0;
+                // c = 0;
                 if (a > 9) {
                     c = a % 10;
                     b = a / 10;
@@ -35,11 +58,11 @@ class AddLargeNumbers {
                     result.push(c);
 
 
-                }
-                else {
+                } else {
                     // a = a + b;
                     // System.out.print(a);
-                    result.push(a);                }
+                    result.push(a);
+                }
 
             }
         }
@@ -49,26 +72,39 @@ class AddLargeNumbers {
 /**
  * solution class.
  **/
-public class Solution {
-    public static void main(String[] args) {
+final class Solution {
+    /**
+     default constructor.
+     **/
+     private Solution() {
+
+     }
+     /**
+      main method.
+      * @param args String.
+      **/
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String p = sc.nextLine();
         String q = sc.nextLine();
-        switch(input){
-            case "numberToDigits":
-                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
-                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
-                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
-                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
-                break;
+        switch (input) {
+        case "numberToDigits":
+            LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+            LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+            System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+            System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+            break;
 
-            case "addLargeNumbers":
-                pDigits = AddLargeNumbers.numberToDigits(p);
-                qDigits = AddLargeNumbers.numberToDigits(q);
-                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-                System.out.println(AddLargeNumbers.digitsToNumber(result));
-                break;
+        case "addLargeNumbers":
+            pDigits = AddLargeNumbers.numberToDigits(p);
+            qDigits = AddLargeNumbers.numberToDigits(q);
+            LinkedList result = AddLargeNumbers.addLargeNumbers(
+                pDigits, qDigits);
+            System.out.println(AddLargeNumbers.digitsToNumber(result));
+            break;
+        default :
+        break;
         }
     }
 
