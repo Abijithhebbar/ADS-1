db1319cad7e1581a447d173c4a513ac8aaa30134
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-04 23:22:24 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
+++ ADS-Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
@@ -1,14 +1,14 @@
 class Node {
     String data;
     Node next;
-    Node(String val){
+    Node(String val) {
         data = val;
     }
 }
-class LinkedList{
+class LinkedList {
     Node start;
     int size;
-    LinkedList(){
+    LinkedList() {
         start = null;
         size = 0;
     }
@@ -16,8 +16,8 @@ class LinkedList{
      * Complexity 1.
      * @param i Node type.
      **/
-    void insert(Node i){
-        if(size == 0){
+    void insert(Node i) {
+        if (size == 0) {
             start = i;
             return;
         }
@@ -30,19 +30,19 @@ class LinkedList{
      * @param index int type.
      * @param n Node type.
      **/
-    void insertAt(int index,Node n) throws Exception{
-        if(index < 0 || index > size){
+    void insertAt(int index, Node n) throws Exception {
+        if (index < 0 || index > size) {
             throw new Exception("");
 
-                    }
+        }
 
-        if(index == 0){
+        if (index == 0) {
             n.next = start;
             start = n;
             size++;
             return;
         }
-        start =insertAt(index, start,n,0);
+        start = insertAt(index, start, n, 0);
 
     }
     /**
@@ -56,37 +56,36 @@ class LinkedList{
      * @param count [description]
      * @return [description]
      **/
-    Node insertAt(int index,Node first, Node element,int count ){
-        if(count == index){
+    Node insertAt(int index, Node first, Node element, int count ) {
+        if (count == index) {
             element.next = first;
             return element;
         }
-        first.next = insertAt(index,first.next,element,count+1);
+        first.next = insertAt(index, first.next, element, count + 1);
         size++;
         return first;
     }
-    void reverse() throws Exception{
-        if(start == null){
+    void reverse() throws Exception {
+        if (start == null) {
             throw new Exception();
         }
-        reverse(null,start);
+        reverse(null, start);
     }
-void reverse( Node prev, Node current)
-{
-   if(current!=null){
-      reverse(current,current.next);
-      current.next = prev;
-    }else{
-        start = prev;
+    void reverse( Node prev, Node current) {
+        if (current != null) {
+            reverse(current, current.next);
+            current.next = prev;
+        } else {
+            start = prev;
+        }
     }
-}
-    void display(){
+    void display() {
         Node temp = start;
-        String str="";
-        while(temp !=null){
-            str += temp.data+", ";
+        String str = "";
+        while (temp != null) {
+            str += temp.data + ", ";
             temp = temp.next;
         }
-        System.out.println(str.substring(0,str.length()-2));
+        System.out.println(str.substring(0, str.length() - 2));
     }
 }
