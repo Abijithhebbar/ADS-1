f1ebfa3e682d8ecc977eac0b68e2893b3f614ac4
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-04 23:32:13 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
+++ ADS-Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
@@ -50,11 +50,11 @@ class LinkedList {
      * @details [long description]
      * insertAt method is used to insert the element at a particular position.
      * Complexity is N.
-     * @param index [description]
-     * @param first [description]
-     * @param element [description]
-     * @param count [description]
-     * @return [description]
+     * @param index int type.
+     * @param first Node type.
+     * @param element Node type.
+     * @param count int type.
+     * @return Node.
      **/
     Node insertAt(int index, Node first, Node element, int count ) {
         if (count == index) {
@@ -65,12 +65,24 @@ class LinkedList {
         size++;
         return first;
     }
+    /**
+    reverse method.
+    It throws an exception if there are no elements.
+    complexity is N.
+     **/
     void reverse() throws Exception {
         if (start == null) {
             throw new Exception();
         }
         reverse(null, start);
     }
+    /**
+    reverse method.
+    It throws an exception if there are no elements.
+    complexity is N.
+     * @param prev Node Type.
+     * @param current Node Type.
+     **/
     void reverse( Node prev, Node current) {
         if (current != null) {
             reverse(current, current.next);
@@ -79,6 +91,10 @@ class LinkedList {
             start = prev;
         }
     }
+    /**
+    Display method.
+    Time complexity Is N.
+     **/
     void display() {
         Node temp = start;
         String str = "";
