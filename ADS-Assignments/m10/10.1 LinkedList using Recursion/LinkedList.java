205588ae472de74bf05b7e0c6ba2492e659a206f
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-04 23:21:41 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
@@ -0,0 +1,92 @@
+class Node {
+    String data;
+    Node next;
+    Node(String val){
+        data = val;
+    }
+}
+class LinkedList{
+    Node start;
+    int size;
+    LinkedList(){
+        start = null;
+        size = 0;
+    }
+    /**
+     * Complexity 1.
+     * @param i Node type.
+     **/
+    void insert(Node i){
+        if(size == 0){
+            start = i;
+            return;
+        }
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * insertAt method.
+     * It throws an exception when elements cant be inserted at the given position.
+     * @param index int type.
+     * @param n Node type.
+     **/
+    void insertAt(int index,Node n) throws Exception{
+        if(index < 0 || index > size){
+            throw new Exception("");
+
+                    }
+
+        if(index == 0){
+            n.next = start;
+            start = n;
+            size++;
+            return;
+        }
+        start =insertAt(index, start,n,0);
+
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * insertAt method is used to insert the element at a particular position.
+     * Complexity is N.
+     * @param index [description]
+     * @param first [description]
+     * @param element [description]
+     * @param count [description]
+     * @return [description]
+     **/
+    Node insertAt(int index,Node first, Node element,int count ){
+        if(count == index){
+            element.next = first;
+            return element;
+        }
+        first.next = insertAt(index,first.next,element,count+1);
+        size++;
+        return first;
+    }
+    void reverse() throws Exception{
+        if(start == null){
+            throw new Exception();
+        }
+        reverse(null,start);
+    }
+void reverse( Node prev, Node current)
+{
+   if(current!=null){
+      reverse(current,current.next);
+      current.next = prev;
+    }else{
+        start = prev;
+    }
+}
+    void display(){
+        Node temp = start;
+        String str="";
+        while(temp !=null){
+            str += temp.data+", ";
+            temp = temp.next;
+        }
+        System.out.println(str.substring(0,str.length()-2));
+    }
+}
---
+++ ADS-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+/**
+ Solution class.
+ **/
+final class Solution{
+    /**
+     default constructor.
+     **/
+    private Solution() {
+
+    }
+    /**
+     * main method.
+     * this method is used to execute the linked list with the recurssion.
+     * @param args [description]
+     **/
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList ll = new LinkedList();
+        while(sc.hasNextLine()){
+            String[] values = sc.nextLine().split(" ");
+            switch (values[0]) {
+                case "insertAt":
+                try{
+                    ll.insertAt(Integer.parseInt(values[1]), new Node(values[2]));
+                ll.display();
+                }
+
+                catch (Exception e) {
+                    System.out.println("Can't insert at this position.");
+
+                }
+                break;
+                case "reverse":
+                try{
+                    ll.reverse();
+                ll.display();
+                }
+
+                catch(Exception e){
+                    System.out.println("No elements to reverse.");
+                }
+                break;
+
+            }
+        }
+    }
+}
