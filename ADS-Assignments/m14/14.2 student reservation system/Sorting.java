a5957fb4f7a3b75490c7226101f7aece0d86d71a
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-09 22:57:01 +0530

    "ads1_m14_a2: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m14/14.2 student reservation system/Sorting.java
+++ ADS-Assignments/m14/14.2 student reservation system/Sorting.java
@@ -93,7 +93,7 @@ class Sorting {
     *@param a Student data type.
     */
     public static void toString(final Studentinfo[] a) {
-        for (int i = 0; i < a.length; i++) {
+        for (int i = a.length - 1; i < 0; i++) {
             System.out.println(a[i].studentName + ","
                 + a[i].totalMarks + ","
                 + a[i].cato);
