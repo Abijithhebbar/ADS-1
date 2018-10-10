330b1eb32caf974aabb5d3427e45674dde034bee
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-10 19:55:26 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-Assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-Assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -3,50 +3,50 @@ import java.util.Scanner;
  * Solution class.
  */
 final class Solution {
-	/**
-	 * default constructor.
-	 */
-	private Solution() {
+    /**
+     * default constructor.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * Main method.
-	 * @param args String.
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String[] input = sc.nextLine().split(" ");
-		Search search = new Search(input.length);
-		for (int i = 0; i < input.length; i++) {
-			search.put(input[i] , i);
-		}
-		while (sc.hasNext()) {
-			String[] inp = sc.nextLine().split(" ");
-			switch (inp[0]) {
-			case "max":
-				System.out.println(search.max());
-				break;
-			case "floor":
-				System.out.println(search.floor(inp[1]));
-				break;
-			case "contains":
-				System.out.println(search.contains(inp[1]));
-				break;
-			case "get":
-				System.out.println(search.get(inp[1]));
-				break;
-			case "keys":
-				search.keys();
-				break;
-			case "rank":
-				System.out.println(search.rank(inp[1]));
-				break;
-			case "deleteMin":
-				search.deleteMin();
-				break;
-			default:
-				break;
-			}
-		}
-	}
+    }
+    /**
+     * Main method.
+     * @param args String.
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        Search search = new Search(input.length);
+        for (int i = 0; i < input.length; i++) {
+            search.put(input[i], i);
+        }
+        while (sc.hasNext()) {
+            String[] inp = sc.nextLine().split(" ");
+            switch (inp[0]) {
+            case "max":
+                System.out.println(search.max());
+                break;
+            case "floor":
+                System.out.println(search.floor(inp[1]));
+                break;
+            case "contains":
+                System.out.println(search.contains(inp[1]));
+                break;
+            case "get":
+                System.out.println(search.get(inp[1]));
+                break;
+            case "keys":
+                search.keys();
+                break;
+            case "rank":
+                System.out.println(search.rank(inp[1]));
+                break;
+            case "deleteMin":
+                search.deleteMin();
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
