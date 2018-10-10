6219d73f27bb9557805de1b8da78effeb6bef546
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-10 22:05:47 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m14/14.2 student reservation system/Solution.java
+++ ADS-Assignments/m14/14.2 student reservation system/Solution.java
@@ -1,37 +1,47 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 /**
- * Solution class.
+ *this the class for main.
  */
- final class Solution {
- 	/**
- 	 * default constructor.lified
- 	 */
- 	private Solution() {
-
- 	}
- 	/**
- 	 * main method.
- 	 * @param args String.
- 	 */
- 	public static void main(final String[] args) {
- 		Scanner scan = new Scanner(System.in);
- 		int qualified = Integer.parseInt(scan.nextLine());
- 		Studentinfo[] students = new Studentinfo[qualified];
- 		int vacancies = Integer.parseInt(scan.nextLine());
- 		int unreserved = Integer.parseInt(scan.nextLine());
- 		int bc = Integer.parseInt(scan.nextLine());
- 		int sc = Integer.parseInt(scan.nextLine());
- 		int st = Integer.parseInt(scan.nextLine());
- 		for (int i = 0; i < qualified; i++) {
- 			String input = scan.nextLine();
- 			Studentinfo eachStudent = new Studentinfo(input);
- 			students[i] = eachStudent;
- 		}
- 		Sorting heap = new Sorting();
-        heap.sort(students);
-        heap.toString(students);
-        System.out.println();
-        heap.meritlist(students, vacancies, unreserved,
-            bc, sc, st);
- 	}
- }
+final class Solution {
+    /**
+     *empty constructor.
+     */
+    private Solution() {
+    }
+    /**
+     * the main to read the input.
+     *
+     * @param  args  The arguments
+     */
+    public static void main(final String[] args) {
+        Student obj = new Student();
+        final int three = 3;
+        final int four = 4;
+        final int five = 5;
+        final int six = 6;
+        Scanner scan = new Scanner(System.in);
+        Database object = new Database();
+        int totalQualified = scan.nextInt();
+        int vacancies = scan.nextInt();
+        int openSeats = scan.nextInt();
+        int bcSeats = scan.nextInt();
+        int scSeats = scan.nextInt();
+        int stSeats = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < totalQualified; i++) {
+            String line  =  scan.nextLine();
+            String[] tokens = line.split(",");
+            obj = new Student(
+                tokens[0], tokens[1], tokens[2],
+                tokens[three], tokens[four],
+                tokens[five], tokens[six]);
+            object.addData(obj);
+        }
+        object.sorting();
+        object.print();
+        object.allotment(vacancies,
+                         openSeats, bcSeats, scSeats, stSeats);
+    }
+}
