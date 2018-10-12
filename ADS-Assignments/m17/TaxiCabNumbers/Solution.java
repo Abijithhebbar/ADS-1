719e928b9e3ed00bbea14f8c39cfaa5fd5138a98
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-12 19:42:11 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m17/TaxiCabNumbers/Solution.java
+++ ADS-Assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,57 +1,38 @@
 import java.util.Scanner;
-class CubeSum implements Comparable<CubeSum> {
-     long sum;
-     int i;
-   int j;
-
-    public CubeSum(int i, int j) {
-        this.sum = i*i*i + j*j*j;
-        this.i = i;
-        this.j = j;
-    }
-
-    public int compareTo(CubeSum that) {
-        if (this.sum < that.sum) return -1;
-        if (this.sum > that.sum) return +1;
-        return 0;
-    }
-
-    public String toString() {
-        return sum + "";
-    }
-
-}
+/**
+class Solution.
+ */
 class Solution {
-     public static void main(String[] args) {
+    /**
+     * Main method.
+     * @param args String.
+     */
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
         int n = 1000;
-
-        // initialize priority queue
         MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
         for (int i = 1; i <= n; i++) {
             pq.insert(new CubeSum(i, i));
         }
 
         int pair = 1;
-        int nth =0;
+        int nth = 0;
 
-        CubeSum previous = new CubeSum(0,0); 
-
-        // find smallest sum, print it out, and update
+        CubeSum previous = new CubeSum(0, 0);
         while (!pq.isEmpty()) {
             CubeSum current = pq.delMin();
-            if(current.sum == previous.sum){
+            if (current.sum == previous.sum) {
                 pair++;
-                if(pair == M){
+                if (pair == M) {
                     nth++;
                 }
                 if (N == nth) {
                     System.out.println(current);
                     break;
                 }
-            }else {
+            } else {
                 pair = 1;
             }
 
@@ -60,5 +41,5 @@ class Solution {
                 pq.insert(new CubeSum(current.i, current.j + 1));
         }
     }
-    
+
 }
