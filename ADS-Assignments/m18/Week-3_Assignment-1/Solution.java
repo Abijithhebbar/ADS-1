bcf0e5ba480feec6b236fa802e47b057df5aaebc
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-13 12:30:05 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS-Assignments/m18/Week-3_Assignment-1/Solution.java
@@ -1,46 +1,119 @@
+import java.util.Scanner;
 /**
- * Solution file for the given program.
+ * Class for list.
  */
- import java.util.Scanner;
- /**
-  * Solution class.
-  */
-  final class Solution {
-  	/**
-  	 * Default constructor.
-  	 */
-  	private Solution() {
-
-  	}
-  	/**
-  	 * Main method of the solution class.
-  	 * @param args String type.
-  	 */
-  	public static void main(final String[] args) {
-  	Scanner sc = new Scanner(System.in);
-  	int n = Integer.parseInt(sc.nextLine());
-  	// Search search = new Search(n);
-  	for (int j = 0; j < 6; j++) {
-  		Search search = new Search(n);
-  		for (int i = 0; i < n; i++) {
-  		String s = sc.nextLine();
-  		String[] lines = s.split(",");
-  		float value = Float.parseFloat(lines[1]);
-  		search.put(value, lines[0]);
-  	}
-  	System.out.println(search.max());
-  	for (int i = 0; i < 4; i++) {
-  		System.out.println(search.max(i));
-  	}
-  	System.out.println("");
-
-  	for (int i = 0; i < 5; i++) {
-  		System.out.println(search.min());
-  		search.deleteMin();
-  	}
-  	if (j != 5) {
-  		System.out.println("");
-  	}
-  	}
-  	}
-  }
+class list implements Comparable<list> {
+    /**
+     * String listName
+     */
+    private String listName;
+    /**
+     * float ListChange.
+     */
+    private float listChange;
+    /**
+     *Constructor.
+     * @param      name   String type.
+     * @param      change  float type.
+     */
+    list(final String name, final float change) {
+        this.listName = name;
+        this.listChange = change;
+    }
+    /**
+     * it is used to get the list name.
+     * @return name.
+     */
+    public String getlistname() {
+        return this.listName;
+    }
+    /**
+     * getlistChange method.
+     * @return float value.
+     */
+    public float getlistchange() {
+        return this.listChange;
+    }
+    /**
+     *
+     * @param      other list type.
+     *
+     * @return     an integer 1 or -1 or 0.
+     */
+    public int compareTo(final list other) {
+        if (this.listChange > other.listChange) {
+            return 1;
+        }
+        if (this.listChange < other.listChange) {
+            return -1;
+        }
+        if (this.listName.compareTo(other.listName) > 0) {
+            return 1;
+        }
+        if (this.listName.compareTo(other.listName) < 0) {
+            return -1;
+        }
+        return 0;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return string which is used to print.
+     */
+    public String toString() {
+        String inputStr = "";
+        inputStr = inputStr + this.listName + " " + this.listChange;
+        return inputStr;
+    }
+}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+    default constructor.
+     */
+    private Solution() {
+    }
+    /**
+     *Main method.
+     * @param      args  String type.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int listLength = Integer.parseInt(scan.nextLine());
+        final int six = 6;
+        final int five = 5;
+        for (int i = 0; i < six; i++) {
+            int count = 0;
+            MinPQ<list> minobj = new MinPQ<>();
+            MaxPQ<list> maxobj = new MaxPQ<>();
+            while (count < listLength) {
+                String[] input = scan.nextLine().split(",");
+                list listObj = new list(input[0],
+                    Float.parseFloat(input[1]));
+                minobj.insert(listObj);
+                maxobj.insert(listObj);
+                count++;
+            }
+            BinarySearchTree<String, Float> bestPerformer
+            = new  BinarySearchTree<>();
+            for (int j = 0; j < five; j++) {
+                list maxpqBest = maxobj.delMax();
+                System.out.println(maxpqBest);
+                bestPerformer.put(maxpqBest.getlistname(),
+                    maxpqBest.getlistchange());
+            }
+            System.out.println();
+            BinarySearchTree<String, Float> worstPerformer
+            = new BinarySearchTree<>();
+            for (int k = 0; k < five; k++) {
+                list minpqWorst = minobj.delMin();
+                System.out.println(minpqWorst);
+                worstPerformer.put(minpqWorst.getlistname(),
+                    minpqWorst.getlistchange());
+            }
+            System.out.println();
+        }
+    }
+}
