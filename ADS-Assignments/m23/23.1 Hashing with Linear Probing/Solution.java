498b3935f109bbdc6841e49b086f33c50e6149c1
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-10-26 18:23:44 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
+++ ADS-Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
@@ -5,43 +5,43 @@
  /**
   * Solution class.
   */
-  final class Solution{
-  	/**
-  	 * default constructor.
-  	 */
-  	private Solution() {
+  final class Solution {
+    /**
+     * default constructor.
+     */
+    private Solution() {
 
-  	}
-  	/**
-  	 * Main function.
-  	 * @param args String.
-  	 */
-  	public static void main(String[] args) {
-  		Scanner sc = new Scanner(System.in);
-  		LinearProbingHashST<String, Integer> hash =
-  		new LinearProbingHashST<String, Integer>();
-  		int n = Integer.parseInt(sc.nextLine());
-  		for (int i = 0; i < n; i++) {
-  			String[] str = sc.nextLine().split(" ");
-  			switch(str[0]) {
-  				case "put":
-  				   hash.put(str[1],Integer.parseInt(str[2]));
-  				break;
-  				case "display":
-  				hash.display();
-  				break;
-  				case "get":
-  				  System.out.println(hash.get(str[1]));
-  				break;
-  				case "delete":
-  				hash.delete(str[1]);
+    }
+    /**
+     * Main function.
+     * @param args String.
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinearProbingHashST<String, Integer> hash =
+        new LinearProbingHashST<String, Integer>();
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] str = sc.nextLine().split(" ");
+            switch (str[0]) {
+                case "put":
+                   hash.put(str[1], Integer.parseInt(str[2]));
+                break;
+                case "display":
+                hash.display();
+                break;
+                case "get":
+                  System.out.println(hash.get(str[1]));
+                break;
+                case "delete":
+                hash.delete(str[1]);
 
-  				break;
-  				default:
-  				break;
-  			}
-  		}
+                break;
+                default:
+                break;
+            }
+        }
 
 
-  	}
+    }
   }
